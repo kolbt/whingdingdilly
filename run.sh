9e5deb56aac1768c552856122fc5db0eea365f84
Thomas Kolb <kolbt@live.unc.edu>
2017-05-08 11:24:30 -0400

    Fixing run.sh

--- run.sh
+++ run.sh
@@ -1,9 +1,8 @@
 #!/bin/sh
-#SBATCH -p gpu                              # partition to run on
+#SBATCH -p general                          # partition to run on
 #SBATCH -n 1                                # number of cores
-#SBATCH -t 0-04:00                          # time (D-HH:MM)
-#SBATCH --mail-type=END,FAIL                # send me email when job finishes or fails
-#SBATCH --mail-user=kolbt@live.unc.edu      # address to send to
+#SBATCH -t 3-00:00                          # time (D-HH:MM)
+
 
 filename=$1
 
