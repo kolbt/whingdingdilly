a1139ece791b2f34912a4c84aebd49cd61286b88
Thomas Kolb <kolbt@live.unc.edu>
2017-07-21 15:48:53 -0400

    Developing a heatmap script which gives number of neighbors of specific type

--- post_proc/analyze.sh
+++ post_proc/analyze.sh
@@ -10,7 +10,8 @@ hoomd_path=$4
 gsd_path=$5
 script_path=$6
 
-python $script_path/post_proc.py $pa $pb $xa $hoomd_path $gsd_path
+python $script_path/heatmap.py $pa $pb $xa $hoomd_path $gsd_path
+#python $script_path/post_proc.py $pa $pb $xa $hoomd_path $gsd_path
 #python $script_path/pp_msd_perc_A.py $pa $pb $xa $hoomd_path $gsd_path
 
 
---
+++ post_proc/heatmap.py
@@ -0,0 +1,99 @@
+import sys
+import hoomd
+from hoomd import md
+from hoomd import deprecated
+
+hoomd_path = str(sys.argv[4])
+gsd_path = str(sys.argv[5])
+
+# need to extract values from filename (pa, pb, xa) for naming
+part_perc_a = int(sys.argv[3])
+part_frac_a = float(part_perc_a) / 100.0
+pe_a = int(sys.argv[1])
+pe_b = int(sys.argv[2])
+
+sys.path.append(hoomd_path)
+
+#initialize system randomly, can specify GPU execution here
+
+part_num = 15000
+
+part_a = part_num * part_frac_a         # get the total number of A particles
+part_a = int(part_a)
+part_b = part_num - part_a              # get the total number of B particles
+part_b = int(part_b)
+
+################################################################################
+############################# Begin Data Analysis ##############################
+################################################################################
+
+sys.path.append(gsd_path)
+import gsd
+from gsd import hoomd
+from gsd import pygsd
+import numpy as np
+import scipy.spatial as spatial
+
+
+myfile = "pa" + str(pe_a) + "_pb" + str(pe_b) + "_xa" + str(part_perc_a) + ".gsd"
+
+f = hoomd.open(name=myfile, mode='rb')
+dumps = f.__len__()
+
+position_array = np.zeros((dumps), dtype=np.ndarray)    # array of position arrays
+type_array = np.zeros((dumps), dtype=np.ndarray)        # particle types
+box_data = np.zeros((1), dtype=np.ndarray)              # box dimensions
+timesteps = np.zeros((dumps), dtype=np.float64)         # timesteps
+
+with hoomd.open(name=myfile, mode='rb') as t:           # open for reading
+    snap = t[0]                                         # snap 0th snapshot
+    box_data = snap.configuration.box                   # get box dimensions
+    for i in range(0,dumps):
+        snap = t[i]                                     # take snap of each dump
+        type_array[i] = snap.particles.typeid
+        position_array[i] = snap.particles.position     # store all particle positions
+        timesteps[i] = snap.configuration.step          # store tstep for plotting purposes
+
+timesteps -= timesteps[0]
+msd_time = timesteps[1:]
+
+from freud import parallel, box, density, cluster
+parallel.setNumThreads(1)                               # don't run multiple threads
+
+l_box = box_data[0]                                     # get box dimensions (square here)
+f_box = box.Box(Lx=l_box,
+                Ly=l_box,
+                is2D=True)                              # initialize freud box
+
+# analyze all particles
+for j in range(0, dumps):
+    
+    l_pos = position_array[j]
+    tree = spatial.KDTree(l_pos)                        # tree of all points
+    a_tree = spatial.KDTree(A_pos)                      # tree of A-type particles
+    b_tree = spatial.KDTree(B_pos)                      # tree of B-type particles
+    radius = 1.0
+    
+    a_neighbors = tree.query_ball_tree(a_tree, radius)
+    b_neighbors = tree.query_ball_tree(b_tree, radius)
+
+
+################################################################################
+#################### Plot the individual and total data ########################
+################################################################################
+
+import matplotlib
+matplotlib.use('Agg')
+import matplotlib.pyplot as plt
+import seaborn as sns
+sns.set(color_codes=True)
+
+plt_name  = "pa" + str(pe_a) + "_pb" + str(pe_b) + "_xa" + str(part_perc_a)
+plt_name1 = "pa" + str(pe_a) + "_pb" + str(pe_b) + "_xa" + str(part_perc_a) + "A"
+plt_name2 = "pa" + str(pe_a) + "_pb" + str(pe_b) + "_xa" + str(part_perc_a) + "B"
+
+if part_perc_a != 0 and part_perc_a != 100:
+    # plot some junk
+
+else:                                                           # if monodisperse plot total values
+
