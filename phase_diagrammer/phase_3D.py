934aa07b589be02711e50112a5c165a53f45d1cc
Thomas Kolb <kolbt@live.unc.edu>
2017-08-01 14:20:04 -0400

    Attempted to plot things in python, was better in mathematica

--- phase_diagrammer/phase_3D.py
+++ phase_diagrammer/phase_3D.py
@@ -2,9 +2,18 @@ import sys
 import matplotlib
 #matplotlib.use('Agg')
 #matplotlib.use('macosx')
+#matplotlib.use('Qt4Agg')
 import matplotlib.pyplot as plt
 import numpy as np
 from mpl_toolkits.mplot3d import Axes3D
+import matplotlib.cm as cm
+from matplotlib.colors import LinearSegmentedColormap
+#from mayavi import mlab
+
+#mlab.test_plot3d()
+#mlab.show()
+
+my_cols = ['b', 'g', 'r', 'c', 'm', 'y', 'b', 'g', 'r', 'c', 'm']
 
 # Some code from stack to plot voxels
 def cuboid_data(pos, size=(1,1,1)):
@@ -29,15 +38,34 @@ def cuboid_data(pos, size=(1,1,1)):
          [o[2], o[2], o[2] + h, o[2] + h, o[2]]]
     return x, y, z
 
-def plotCubeAt(pos=(0,0,0),ax=None):
+def plotCubeAt(pos=(0,0,0),ax=None, z_ind=0):
     # Plotting a cube element at position pos
     if ax !=None:
         X, Y, Z = cuboid_data( pos )
-        ax.plot_surface(X, Y, Z, color='b', rstride=1, cstride=1, alpha=0.8)
+        ax.plot_surface(X, Y, Z, color=my_cols[z_ind], rstride=1, cstride=1, alpha=0.8)
 
 # start with 1D array
+
+'''
+    The data you are drawing from is stacked in set Pe_B planes. So,
+    rows 1-10 (that's 11 rows) are all binary values which indicate
+    phase separation ( 0 = no, 1 = yes) at Pe_B = 0.  Thus, rows 
+    11-21 are for Pe_B = 10 etc. etc. until Pe_B = 150 (16 total
+    planes).
+    
+    Ultimately, Mathematica was better at this, check out 
+    "exp_theory_overlay.nb"
+'''
+
 file = str(sys.argv[1])
 data = np.loadtxt(file, dtype=np.int8)
+
+peb = np.zeros((16), dtype=np.ndarray)
+var = 0
+for i in range(0,16):
+    peb[i] = data[var:var+11, :16]
+    var += 11
+    print(peb[i])
 
 # for loop the fuck outta this
 #phase_3D = np.zeros((11,16,16), dtype=np.int8)
@@ -76,12 +104,21 @@ ax = fig.add_subplot(111, projection='3d')
 ##            else:
 ##                ax.scatter(idx, idy, idz, c='w')
 
+#my_cols = np.arange(11.0)
+#my_cols /= 10.0
+#colors = [(1, 0, 0), (0, 1, 0), (0, 0, 1)]
+#my_cm = LinearSegmentedColormap.from_list('my_map', my_cols, N=10)
 for idx in range(0,16):
     for idy in range(0,16):
         for idz in range(0,11):
             if phase_3D[idx,idy,idz] != 0:
-                #                ax.scatter(idx, idy, idz, c='b', marker='s', s=20)
-                plotCubeAt(pos=(idx-0.5, idy-0.5, idz-0.5), ax=ax)
+                ax.scatter(idx, idy, idz, c=my_cols[idz], s=40, alpha=0.8, depthshade=True)
+
+#                if idz % 2 == 0:
+#                    ax.scatter(idx, idy, idz, marker='.', s=40, c='b', depthshade=True)
+#                else:
+#                    ax.scatter(idx, idy, idz, marker='.', s=40, c='k', depthshade=True)
+#                plotCubeAt(pos=(idx-0.5, idy-0.5, idz-0.5), ax=ax, z_ind=idz)
 #            else:
 #                ax.scatter(idx, idy, idz, c='w')
 
@@ -93,15 +130,20 @@ for idx in range(0,16):
 #ax.set_zlim3d(0,15)
 #plt.show()
 
-
+#ax.controuf()
 ax.set_xlabel('Pe_A')
 ax.set_xlim3d(0,15)
 ax.set_ylabel('Pe_B')
 ax.set_ylim3d(0,15)
 ax.set_zlabel('Particle Fraction of A')
 ax.set_zlim3d(0,10)
+#plt.colorbar()
 plt.show()
 
 # WORD. Now plot that shit
 #plt.plot(phase_3D, cmap='Blues')
 fig.savefig('phase_3D.png', dpi=1000)
+plt.close()
+
+#mlab.contour3d(phase_3D, contours=5)
+
