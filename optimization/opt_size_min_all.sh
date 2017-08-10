b71209bd086c886f04497c063bde246eca38984e
Thomas Kolb <kolbt@live.unc.edu>
2017-08-10 13:20:38 -0400

    Optimized the cluster size cut off at 1000

--- optimization/opt_size_min_all.sh
+++ optimization/opt_size_min_all.sh
@@ -5,7 +5,7 @@ gsd_path='/Users/kolbt/Desktop/compiled/gsd/build'
 script_path='/Users/kolbt/Desktop/compiled/whingdingdilly/optimization'
 sedtype='gsed'
 
-size_min=50
+size_min=1500
 
 for filename in $( ls pa*.gsd )
 do
@@ -15,7 +15,7 @@ do
     pb=$(echo $filename | $sedtype 's/^.*pb\([0-9]*\)_.*/\1/')
     xa=$(echo $filename | $sedtype 's/^.*xa\([0-9]*\)..*/\1/')
 
-    while [ $size_min -le 1000 ]
+    while [ $size_min -le 5000 ]
     do
 
         echo "Running minimum cluster size of ${size_min}... "
@@ -23,12 +23,13 @@ do
         python ${script_path}/optimize_size_min.py\
         $pa $pb $xa $hoomd_path $gsd_path $size_min
         # Run video creation command
-        ffmpeg -framerate 20 -i sm_${size_min}_opt_%03d.png -y sm_${size_min}_opt.mp4
+        ffmpeg -y -framerate 20 -i sm_${size_min}_opt_%03d.png -vcodec libx264\
+        -pix_fmt yuv420p -vf "crop=in_w-180:in_h" sm_${size_min}_opt.mov
         # Increment size_min
-        size_min=$(( $size_min + 50 ))
+        size_min=$(( $size_min + 500 ))
 
     done
 
-    size_min=$(( 50 ))
+    size_min=$(( 1500 ))
 
 done
