880879eb7754a38ddc4144ac175b3cadaeb9cec1
Thomas Kolb <kolbt@live.unc.edu>
2017-08-11 15:26:04 -0400

    Fix gpu execution

--- run_gpu.sh
+++ run_gpu.sh
@@ -4,7 +4,7 @@
 #SBATCH --partition=gpu                     # partition to run on
 #SBATCH --mem=8G                            # memory requested
 #SBATCH --nodes=1                           # run on one node
-#SBATCH --ntasks=1                          # task count
+#SBATCH --ntasks=2                          # task count
 #SBATCH --cpus-per-task=4                   # core count
 #SBATCH --time=11-00:00                     # time (D-HH:MM)
 
