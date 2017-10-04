ceba037911dc271f3d30e4e4932a426575e18d0f
Thomas Kolb <kolbt@live.unc.edu>
2017-10-04 13:52:58 -0400

    Added plot overlay for MSD/MCS. Need to fix MSD out frequency

--- gnuplot/gnuplotter.sh
+++ gnuplot/gnuplotter.sh
@@ -1,2 +1,39 @@
 #!bin/sh
 
+path='/Users/kolbt/Desktop/compiled/whingdingdilly/gnuplot/'
+xa=0
+pb=0
+
+#while [ $xa -le 100 ]
+#do
+#
+#    cp ${path}MSD_template_gnuplot.txt MSD_gnuplot_xa${xa}.txt
+#    cp ${path}MCS_template_gnuplot.txt MCS_gnuplot_xa${xa}.txt
+#
+#    for file in $(ls LOG*MCS*xa${xa}*.txt)
+#    do
+#
+#        echo "\'$file\' using 1:2" >> MCS_gnuplot_xa${xa}.txt
+#
+#    done
+#
+#    for file in $(ls *MSD*ten*xa${xa}*.txt)
+#    do
+#
+#        echo "\'$file\' using 1:2" >> MSD_gnuplot_xa${xa}.txt
+#
+#    done
+#
+#    xa=$(( $xa + 10 ))
+#
+#done
+
+while [ $xa -le 100 ]
+do
+
+    cp ${path}MSD_temp.txt MSD_gnuplot_xa${xa}.txt
+    gsed -i 's/\${xa}/'"${xa}"'/g' MSD_gnuplot_xa${xa}.txt
+    gsed -i 's/\${pb}/'"${pb}"'/g' MSD_gnuplot_xa${xa}.txt
+    xa=$(( $xa + 10 ))
+
+done
