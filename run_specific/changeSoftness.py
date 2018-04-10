1964fddd58ca03e39d676d7b188bed22d629b37f
Thomas Kolb <kolbt@live.unc.edu>
2018-04-10 15:24:27 -0400

    Naming issue

--- run_specific/changeSoftness.py
+++ run_specific/changeSoftness.py
@@ -229,7 +229,8 @@ else:
 # Get filenames for various file types
 name = "pa" + str(peA) +\
 "_pb" + str(peB) +\
-"_xa" + str(partPercA)
+"_xa" + str(partPercA) +\
+"_ep" + str(eps)
 
 gsdName = name + ".gsd"
 sqliteName = name + ".sqlite"
