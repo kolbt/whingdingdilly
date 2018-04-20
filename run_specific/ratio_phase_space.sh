5bfeacbe95b7a1ab1bea31cf8ba34653cab18fea
Thomas Kolb <kolbt@live.unc.edu>
2018-04-20 13:20:17 -0400

    Submit all phase space simulations

--- run_specific/ratio_phase_space.sh
+++ run_specific/ratio_phase_space.sh
@@ -1,5 +1,8 @@
 #!/bin/sh
 
+current=$( date "+%m_%d_%y" )
+this_path=$( pwd )
+
 echo "Are you running on Longleaf (y/n)?"
 read answer
 
@@ -28,7 +31,7 @@ if [ $gpu == "y" ]; then
 fi
 
 # Default values for simulations
-part_num=$(( 15000 ))
+part_num=$(( 30000 ))
 phi=$(( 60 ))
 runfor=$(( 100 ))
 dump_freq=$(( 20000 ))
@@ -44,23 +47,77 @@ peB_start=$(( 0 ))  # minimum peB value
 peB_max=$(( 150 ))  # maximum peB value
 
 # Ask for input values
+#loop="n"
+#while [ $loop == "n" ]
+#do
+#
+#    echo "part_num is ${part_num}, input new value or same value."
+#    read part_num
+#    echo "density, as a percent, is ${phi}."
+#    read phi
+#    echo "runfor is ${runfor} tau, input new value or same value."
+#    read runfor
+#    echo "dump_freq is ${dump_freq}, input new value or same value."
+#    read dump_freq
+#    echo "xA_start is ${xA_start}, input new value or same value."
+#    read xA_start
+#    echo "xA_max is ${xA_max}, input new value or same value."
+#    read xA_max
+#    echo "xA_spacer is ${xA_spacer}, input new value or same value."
+#    read xA_spacer
+#    echo "peA_start is ${peA_start}, input new value or same value."
+#    read peA_start
+#    echo "peA_max is ${peA_max}, input new value or same value."
+#    read peA_max
+#    echo "peA_spacer is ${peA_spacer}, input new value or same value."
+#    read peA_spacer
+#    echo "peB_start is ${peB_start}, input new value or same value."
+#    read peB_start
+#    echo "peB_max is ${peB_max}, input new value or same value."
+#    read peB_max
+#    echo "peB_spacer is ${peB_spacer}, input new value or same value."
+#    read peB_spacer
+#
+#    # this shows how long the simulation will run
+#    tsteps=$(bc <<< "scale=2;$runfor/0.000001")
+#
+#    echo "part_num is ${part_num}"
+#    echo "runfor is ${runfor}"
+#    echo "dump_freq is ${dump_freq}"
+#    echo "xA_start is ${xA_start}"
+#    echo "xA_max is ${xA_max}"
+#    echo "xA_spacer is ${xA_spacer}"
+#    echo "peA_start is ${peA_start}"
+#    echo "peA_max is ${peA_max}"
+#    echo "peA_spacer is ${peA_spacer}"
+#    echo "peB_start is ${peB_start}"
+#    echo "peB_max is ${peB_max}"
+#    echo "peB_spacer is ${peB_spacer}"
+#    echo "Simulation will run for ${tsteps} timesteps"
+#
+#    echo "Are these values okay (y/n)?"
+#    read loop
+#
+#done
 
 # Ask for seeds
-#echo "Time to set some seeds!"
-#echo "Positional seed"
-#read seed1
-#echo "Equilibration seed"
-#read seed2
-#echo "Orientational seed"
-#read seed3
-#echo "A activity seed"
-#read seed4
-#echo "B activity seed"
-#read seed5
+echo "Time to set some seeds!"
+echo "Positional seed"
+read seed1
+echo "Equilibration seed"
+read seed2
+echo "Orientational seed"
+read seed3
+echo "A activity seed"
+read seed4
+echo "B activity seed"
+read seed5
+
+mkdir ${current}_parent
+cd ${current}_parent
 
 # Submission routine (no duplicates)
 peB_current=$(( $peB_start ))
-count=$(( 0 ))
 # While loop for faster activity species
 while [ $peB_current -le $peB_max ]
 do
@@ -74,11 +131,28 @@ do
         # While loop for particle fraction (not monodisperse)
         while [ $xA_current -le $xA_max ]
         do
-            # Submit simulations with given parameters
+
             echo "PeB = ${peB_current}, PeA = ${peA_current}, xA = ${xA_current}"
+            # Submit simulations with given parameters
+            infile=pa${peA_current}_pb${peB_current}_xa${xA_current}.py                          # set unique infile name
+            $sedtype -e 's@\${hoomd_path}@'"${hoomd_path}"'@g' $template > $infile  # write path to infile (delimit with @)
+            $sedtype -i 's/\${part_num}/'"${part_num}"'/g' $infile                  # write particle number
+            $sedtype -i 's/\${phi}/'"${phi}"'/g' $infile                            # write particle number
+            $sedtype -i 's/\${runfor}/'"${runfor}"'/g' $infile                      # write time in tau to infile
+            $sedtype -i 's/\${dump_freq}/'"${dump_freq}"'/g' $infile                # write dump frequency to infile
+            $sedtype -i 's/\${part_frac_a}/'"${xA_current}"'/g' $infile             # write particle fraction to infile
+            $sedtype -i 's/\${pe_a}/'"${peA_current}"'/g' $infile                     # write activity of A to infile
+            $sedtype -i 's/\${pe_b}/'"${peB_current}"'/g' $infile                     # write activity of B to infile
+            $sedtype -i 's@\${gsd_path}@'"${gsd_path}"'@g' $infile                  # set gsd path variable
+            $sedtype -i 's/\${seed1}/'"${seed1}"'/g' $infile                        # set your seeds
+            $sedtype -i 's/\${seed2}/'"${seed2}"'/g' $infile
+            $sedtype -i 's/\${seed3}/'"${seed3}"'/g' $infile
+            $sedtype -i 's/\${seed4}/'"${seed4}"'/g' $infile
+            $sedtype -i 's/\${seed5}/'"${seed5}"'/g' $infile
+            $submit $script_path $infile
+
             # Increment particle fraction
             xA_current=$(( $xA_current + $xA_spacer ))
-            count=$(( $count + 1 ))
 
         done
         # Increment activity of species A
@@ -89,5 +163,3 @@ do
     peB_current=$(( $peB_current + $peB_spacer ))
 
 done
-
-echo "Ran ${count} times."
