4f5a684b3fa49ec8b4b63fd9697512e52dbd5c97
Thomas Kolb <kolbt@live.unc.edu>
2018-05-01 13:46:57 -0400

    Resubmission script and larger memory for failed simulations

--- run.sh
+++ run.sh
@@ -1,7 +1,8 @@
 #!/bin/sh
 #SBATCH -p general                          # partition to run on
 #SBATCH -n 1                                # number of cores
-#SBATCH -t 10-00:00                          # time (D-HH:MM)
+#SBATCH -t 10-00:00                         # time (D-HH:MM)
+#SBATCH --mem=32G                           # memory requested
 
 
 filename=$1
