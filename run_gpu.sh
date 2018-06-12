6df26b3e58814bfb555b72caba987fbc4ce87c56
Thomas Kolb <kolbt@live.unc.edu>
2018-06-12 09:37:04 -0400

    GPU no longer broken, run wherever ya like

--- run_gpu.sh
+++ run_gpu.sh
@@ -3,7 +3,6 @@
 #SBATCH --gres=gpu:1                        # I want one gpus
 #SBATCH --partition=gpu                     # partition to run on
 #SBATCH --nodes=1                           # run on one node
-#SBATCH --nodelist=g0603                    # specific gpu to run on
 #SBATCH --time=11-00:00                     # time (D-HH:MM)
 
 filename=$1
