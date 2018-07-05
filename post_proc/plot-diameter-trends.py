4a76faaebff999c1efa2a77eebc8e2f07e06330a
Thomas Kolb <kolbt@live.unc.edu>
2018-07-05 13:53:30 -0400

    Need to group data that has the same xA and peFast

--- post_proc/plot-diameter-trends.py
+++ post_proc/plot-diameter-trends.py
@@ -44,14 +44,55 @@ color: activity of fast species
 shape: particle fraction
 '''
 
-#for loop for color
-for i in range(0, len(peFast)):
-    
-    #for loop for shape
-    for j in range(0, len(xSlow)):
-        
-        #plot the point
-        plt.scatter(peR, phiEff)
+# Get distinct PeR values
+peRs = []
+for i in range(0, len(peR)):
+    if not (peR[i] in peRs):
+        peRs.append(peR[i])
+
+# Get distinct PeFast values
+peFast = []
+for i in range(0, len(peB)):
+    if not (peB[i] in peFast):
+        peFast.append(peB[i])
+# colors = np.arange(0, len(peFast))
+
+# Get distinct xSlow values
+xSlow = []
+for i in range(0, len(xA)):
+    if not (xA[i] in xSlow):
+        xSlow.append(xA[i])
+# shapes = np.arange(0, len(xSlow))
+
+# # Loop through PeR
+# for h in range(0, len(peR)):
+#     # Get index for PeFast/ set color
+#     color = 0
+#     for i in range (0, len(peFast)):
+#         if peB[h] == peFast[i]:
+#             color = i
+#     # Get index for xSlow/ set shape
+#     shape = 0
+#     for i in range(0, len(xSlow)):
+#         if xA[h] == xSlow[i]:
+#             shape = i
+#     plt.scatter(peR[h], phiEff[h], c=color, s=shape)
+
+# Need to connect lines that have same PeFast and xSlow
+lines = np.zeros( (len(peFast) * len(xSlow), len(peRs) ), dtype=np.float64)
+
+# Loop through all data points
+for i in range(0, phiEff):
+    for i in range(0, )
+
+
+
+# #for loop for color
+# for i in range(0, len(peFast)):
+#     #for loop for shape
+#     for j in range(0, len(xSlow)):
+#         #plot the point
+#         plt.scatter(peR[g], phiEff[h], c=colors[i], s=shapes[j])
 
 plt.ylim(0.45, 0.60)
 plt.xlabel(r'$Pe_{Ratio}$')
