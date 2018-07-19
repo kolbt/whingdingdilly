7ca590f5132f7ff0138e739c0b8808435b7e3977
Thomas Kolb <kolbt@live.unc.edu>
2018-07-19 12:30:03 -0400

    Made the plotting overlay

--- phase_diagrammer/plot-phase-data.py
+++ phase_diagrammer/plot-phase-data.py
@@ -16,8 +16,10 @@ import numpy as np
 # import matplotlib
 # matplotlib.use('Agg')
 import matplotlib.pyplot as plt
+from textwrap import wrap
 
 phaseTxt = sys.argv[1]
+catesTxt = '/Users/kolbt/Desktop/compiled/whingdingdilly/phase_diagrammer/cates_phase_data.txt'
 
 # Import data into arrays
 peA, \
@@ -25,15 +27,53 @@ peB, \
 xA, \
 ps = np.loadtxt(phaseTxt, skiprows=1, unpack=True)
 
+CpeA, \
+CpeB, \
+CxA, \
+Cps = np.loadtxt(catesTxt, skiprows=1, unpack=True)
+
 xA /= 100.0
+CxA /= 100.0
 
+# It'd be best to sort entries and use same index
+
+# for i in range(0, len(peA)):
+#     # Find matching Cates value
+#     for j in range(0, len(Cps)):
+#         if CpeA[j] == peA[i]:
+#             for k in range(0, len(Cps)):
+#                 if CxA[k] == xA[i]:
+#                     print(Cps[k])
+#                     if Cps[k] == ps[i] == 1:
+#                         plt.scatter(peA[i], xA[i], facecolor='k', edgecolor='k')
+#                     elif Cps[k] == ps[i] == 0:
+#                         plt.scatter(peA[i], xA[i], facecolor='none', edgecolor='k')
+#                     elif Cps[k] == 0 and ps[i] == 1:
+#                         plt.scatter(peA[i], xA[i], facecolor='r', edgecolor='k')
+#                     else:
+#                         plt.scatter(peA[i], xA[i], facecolor='b', edgecolor='k')
+#                     break
+
+# Plot Cates data
+for i in range(0, len(CpeA)):
+    if Cps[i] == 1:
+        black = plt.scatter(CpeA[i], CxA[i], facecolor='k', edgecolor='k')
+    else:
+        empty = plt.scatter(CpeA[i], CxA[i], facecolor='none', edgecolor='k')
+
+# Plot my data on top of it (hacky but it works)
 for i in range(0, len(peA)):
-    if ps[i] == 1:                                      # is phase sep.
-        plt.scatter(peA[i], xA[i], facecolor='k', edgecolor='k')
-    else:                                               # is not phase sep.
-        plt.scatter(peA[i], xA[i], facecolor='none', edgecolor='k')
+    if ps[i] == 1:
+        blue = plt.scatter(peA[i], xA[i], facecolor='b', edgecolor='k')
 
-plt.title(r'Active/Passive Phase Separation $\epsilon=1$')
+legend = plt.legend(bbox_to_anchor=(0.0, 1.0),
+                    loc='center left',
+                    ncol=3,
+                    handles=[empty, black, blue],
+                    labels=['Gas', 'HS: Phase Separated', r'$\epsilon=1:$ Phase Separated'],
+                    framealpha=1.0)
+plt.title(r'Active/Passive Phase Separation', y=1.03)
 plt.xlabel(r'$Pe_{Fast}$')
 plt.ylabel(r'$x_{Fast}$')
+plt.tight_layout()
 plt.savefig('phase-seperation-plot.png', dpi=1000)
