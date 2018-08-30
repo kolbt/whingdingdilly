0bf7ec53cf46faf2f22fcebfc7b18d98ae95746f
Thomas Kolb <kolbt@live.unc.edu>
2018-08-30 12:01:01 -0400

    Reduce number of dumps

--- run_specific/changeSoftness.py
+++ run_specific/changeSoftness.py
@@ -99,7 +99,7 @@ dt = 0.000001 * tauLJ                        # timestep size
 simLength = runFor * tauBrown               # how long to run (in tauBrown)
 simTauLJ = simLength / tauLJ                # how long to run (in tauLJ)
 totTsteps = int(simLength / dt)             # how many tsteps to run
-numDumps = float(simLength / 0.05)           # dump data every 0.1 tauBrown
+numDumps = float(simLength / 0.5)           # dump data every 0.1 tauBrown
 dumpFreq = float(totTsteps / numDumps)      # normalized dump frequency
 dumpFreq = int(dumpFreq)                    # ensure this is an integer
 
