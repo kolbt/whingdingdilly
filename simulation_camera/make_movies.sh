109c11ec26b0994379ed3dd4dc3935469bb1808a
Thomas Kolb <kolbt@live.unc.edu>
2018-10-23 10:37:02 -0400

    Movie-making edits

--- simulation_camera/make_movies.sh
+++ simulation_camera/make_movies.sh
@@ -3,7 +3,7 @@
 in_path=$1
 script_path="/Users/kolbt/Desktop/compiled/whingdingdilly/simulation_camera"
 
-for sim in $(ls ${in_path}/*.gsd)
+for sim in $(ls ${in_path}/pa20_pb70_xa*.gsd)
 do
 
     ovitos ${script_path}/make_movie.py ${sim}
