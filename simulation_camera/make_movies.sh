2ec0544834bb479548170a4f31efb6b3ffa2e9f2
Thomas Kolb <kolbt@live.unc.edu>
2018-11-29 10:17:54 -0500

    Need to analyze histograms, could delta be a bit larger than the diameter?

--- simulation_camera/make_movies.sh
+++ simulation_camera/make_movies.sh
@@ -3,7 +3,7 @@
 in_path=$1
 script_path="/Users/kolbt/Desktop/compiled/whingdingdilly/simulation_camera"
 
-for sim in $(ls ${in_path}/pa0_pb500_xa*.gsd)
+for sim in $(ls ${in_path}/pa*.gsd)
 do
 
     ovitos ${script_path}/make_movie.py ${sim}
