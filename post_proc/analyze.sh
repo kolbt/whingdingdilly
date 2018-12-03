403a197ecbf8852b627e28cfafc5f5dc19c2d97f
Thomas Kolb <kolbt@live.unc.edu>
2018-12-03 11:51:12 -0500

    Need to check the validity of the cluster thresholds

--- post_proc/analyze.sh
+++ post_proc/analyze.sh
@@ -34,9 +34,10 @@ ep=$7
 #python $script_path/mesh_nearest_neighbor.py $pa $pb $xa $hoomd_path $gsd_path
 #python $script_path/computeRDF.py $pa $pb $xa $hoomd_path $gsd_path $ep
 #python $script_path/extrinsic_txt.py $pa $pb $xa $hoomd_path $gsd_path $ep
-python $script_path/extrinsic_all_time.py $pa $pb $xa $hoomd_path $gsd_path $ep
+#python $script_path/extrinsic_all_time.py $pa $pb $xa $hoomd_path $gsd_path $ep
 #python $script_path/edge_detection.py $pa $pb $xa $hoomd_path $gsd_path
 #python $script_path/edge_detection_v2.py $pa $pb $xa $hoomd_path $gsd_path
+python $script_path/check_cluster_alg.py $pa $pb $xa $hoomd_path $gsd_path $ep
 
 #ffmpeg -framerate 10 -i mvy_pa${pa}_pb${pb}_xa${xa}_%d.png\
 # -vcodec libx264 -s 1000x1000 -pix_fmt yuv420p -threads 1\
---
+++ post_proc/check_cluster_alg.py
@@ -0,0 +1,217 @@
+'''
+#                           This is an 80 character line                       #
+Summary:  Read in simulation gsds, output image of clustered particles
+'''
+
+import sys
+
+pe_a = int(sys.argv[1])                     # activity A
+pe_b = int(sys.argv[2])                     # activity B
+part_perc_a = int(sys.argv[3])              # percentage A particles
+part_frac_a = float(part_perc_a) / 100.0    # fraction A particles
+hoomd_path = str(sys.argv[4])               # local path to hoomd-blue
+gsd_path = str(sys.argv[5])                 # local path to gsd
+
+sys.path.append(hoomd_path)     # ensure hoomd is in your python path
+sys.path.append(gsd_path)       # ensure gsd is in your python path
+
+import hoomd
+from hoomd import md
+from hoomd import deprecated
+
+import gsd
+from gsd import hoomd
+from gsd import pygsd
+
+import freud
+from freud import parallel
+from freud import box
+from freud import density
+from freud import cluster
+
+import numpy as np
+from scipy import stats
+
+def computeR(part1, part2):
+    """Computes distance"""
+    return np.sqrt(((part2[0]-part1[0])**2)+((part2[1]-part1[1])**2))
+
+def computeA(diameter):
+    """Computes area of circle"""
+    radius = diameter / 2.0
+    return np.pi * (radius**2)
+
+def getDistance(point1, point2x, point2y):
+    """Find the distance between two points"""
+    distance = np.sqrt((point2x - point1[0])**2 + (point2y - point1[1])**2)
+    return distance
+
+def slowSort(array):
+    """Sort an array the slow (but certain) way"""
+    cpy = np.copy(array)
+    ind = np.arange(0, len(array))
+    for i in xrange(len(cpy)):
+        for j in xrange(len(cpy)):
+            if cpy[i] > cpy[j] and i < j:
+                # Swap the copy array values
+                tmp = cpy[i]
+                cpy[i] = cpy[j]
+                cpy[j] = tmp
+                # Swap the corresponding indices
+                tmp = ind[i]
+                ind[i] = ind[j]
+                ind[j] = tmp
+    return ind
+
+def indSort(arr1, arr2):
+    """Take sorted index array, use to sort array"""
+    # arr1 is array to sort
+    # arr2 is index array
+    cpy = np.copy(arr1)
+    for i in xrange(len(arr1)):
+        arr1[i] = cpy[arr2[i]]
+
+def chkSort(array):
+    """Make sure sort actually did its job"""
+    for i in xrange(len(array)-2):
+        if array[i] > array[i+1]:
+            print("{} is not greater than {} for indices=({},{})").format(array[i+1], array[i], i, i+1)
+            return False
+    return True
+
+try:
+    # This is for the long timescale data
+    in_file = "pa" + str(pe_a) + \
+                "_pb" + str(pe_b) + \
+                "_xa" + str(part_perc_a) + \
+                ".gsd"
+    # File to write all data to
+    out_file = "pa" + str(pe_a) + \
+               "_pb" + str(pe_b) + \
+               "_xa" + str(part_perc_a) + \
+               "_frame"
+    f = hoomd.open(name=in_file, mode='rb') # open gsd file with hoomd
+except:
+    try:
+        eps = str(sys.argv[6])
+    except:
+        eps = 1
+        
+    in_file = "pa" + str(pe_a) + \
+                "_pb" + str(pe_b) + \
+                "_xa" + str(part_perc_a) + \
+                "_ep" + str(eps) + \
+                ".gsd"
+    # File to write all data to
+    out_file = "pa" + str(pe_a) + \
+               "_pb" + str(pe_b) + \
+               "_xa" + str(part_perc_a) + \
+               "_ep" + str(eps) +\
+               "_frame"
+    f = hoomd.open(name=in_file, mode='rb')  # open gsd file with hoomd
+
+dump_long = int(f.__len__())                # get number of timesteps dumped
+
+start = 0                       # gives first frame to read
+end = dump_long                 # gives last frame to read
+
+positions = np.zeros((end), dtype=np.ndarray)       # array of positions
+types = np.zeros((end), dtype=np.ndarray)           # particle types
+box_data = np.zeros((1), dtype=np.ndarray)          # box dimensions
+timesteps = np.zeros((end), dtype=np.float64)       # timesteps
+
+# Get relevant data from long.gsd file
+with hoomd.open(name=in_file, mode='rb') as t:
+    snap = t[0]
+    for iii in range(start, end):
+        snap = t[iii]                  # snapshot of frame
+        types[iii] = snap.particles.typeid          # get types
+        positions[iii] = snap.particles.position    # get positions
+        timesteps[iii] = snap.configuration.step    # get timestep
+
+# Get index order for chronological timestep sorting
+newInd = slowSort(timesteps)
+# Use these indexes to reorder other arrays
+indSort(timesteps, newInd)
+indSort(positions, newInd)
+indSort(types, newInd)
+
+if chkSort(timesteps):
+    print("Array succesfully sorted")
+else:
+    print("Array not sorted")
+timesteps -= timesteps[0]
+
+# Get number of each type of particle
+partNum = len(types[start])
+part_A = int(partNum * part_frac_a)
+part_B = partNum - part_A
+
+# Feed data into freud analysis software
+l_box = box_data[0]
+h_box = l_box / 2.0
+a_box = l_box * l_box
+f_box = box.Box(Lx = l_box, Ly = l_box, is2D = True)    # make freud box
+my_clust = cluster.Cluster(box = f_box,                 # init cluster class
+                           rcut = 1.02)                 # distance to search
+c_props = cluster.ClusterProperties(box = f_box)        # compute properties
+
+# Parameters for sorting dense from dilute
+min_size_abso = 1000
+min_size_perc = 0.05 * partNum  # minimum cluster size 5% of all particles
+min_size = min_size_abso if min_size_abso < min_size_perc else min_size_perc
+
+min_size = 2
+
+for j in range(start, end):
+
+    # Easier accessors
+    pos = positions[j]
+    typ = types[j]
+    tst = timesteps[j]
+
+    # Run freud computations
+    my_clust.computeClusters(pos)           # feed in position
+    ids = my_clust.getClusterIdx()          # get id of each cluster
+    c_props.computeProperties(pos, ids)     # find cluster properties
+    clust_size = c_props.getClusterSizes()  # find cluster sizes
+
+    # Querry array, that tells whether cluster ID is of sufficient size
+    q_clust = np.zeros((len(clust_size)), dtype=np.int)
+    clust_num = 0   # number of clusters
+    lcIndex = 0     # id of largest cluster
+    lc_numA = 0     # number of A particles in dense phase
+    lc_numB = 0     # number of B particles in dense phase
+
+    # This sorts clusters that are too small
+    for k in range(0, len(clust_size)):
+        if clust_size[k] > min_size:
+            q_clust[k] = 1
+            clust_num += 1
+        if clust_size[k] > l_clust:
+            l_clust = clust_size[k]
+            lcIndex = k
+
+    # Plot the data
+    for k in range(0, partNum):
+        # This is in gas phase
+        if q_clust[ids[k]] == 1:
+            gas_num += 1
+            if typ[k] == 0:
+                gas_A += 1
+                plt.scatter(pos[k][0], pos[k][1], facecolor='w', edgecolors='#FF00FF')
+            else:
+                gas_B += 1
+                plt.scatter(pos[k][0], pos[k][1], facecolor='w', edgecolors='#6BF648')
+        # This is in dense phase
+        else:
+            dense_num += 1
+            if typ[k] == 0:
+                dense_A += 1
+                plt.scatter(pos[k][0], pos[k][1], facecolor='#FF00FF', edgecolors='#FF00FF')
+            else:
+                dense_B += 1
+                plt.scatter(pos[k][0], pos[k][1], facecolor='#6BF648', edgecolors='#6BF648')
+    plt.xlim(-h_box, h_box)
+    plt.ylim(-h_box, h_box)
+    plt.savefig(out_file + str(j) + '.png', dpi=1000)
