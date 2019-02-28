f0884506bc36c53a4dc1c72b84f94db4ecec7a40
Thomas Kolb <kolbt@live.unc.edu>
2019-02-28 09:39:34 -0500

    Wrote a script that will add my monodisperse simulations to the phase plane diagram

--- simulation_camera/addMonoToPlanes.sh
+++ simulation_camera/addMonoToPlanes.sh
@@ -7,7 +7,7 @@ size=600
 pea=0
 
 # Loop through B particle activity
-while [ ${pea} -le 20 ]; do
+while [ ${pea} -le 150 ]; do
 
     # Frame to place the monodisperse simulation on
     composite="pb${pea}.png"
@@ -19,11 +19,12 @@ while [ ${pea} -le 20 ]; do
     done
     # Get rid of .gsd suffix
     base=${file%.gsd}
-    # Create the frame
+    # Create the frame (with green particles)
     ovitos ${camPath}/png_final_tstep.py $file 1 $size
 
-    xa=10
+
     # This places the monodisperse simulation in the vertical column (PeA=PeB)
+    xa=10
     while [ ${xa} -le 90 ]; do
         # y-position on composite
         y=$(bc <<< "scale=0;((($xa/10))*$size)")
@@ -33,17 +34,41 @@ while [ ${pea} -le 20 ]; do
         xa=$(( $xa + 10 ))
     done
 
+
     # This places the bottom row of frames (xA=0)
-    while [ ${xa} -le 90 ]; do
-        x=$(bc <<< "scale=0;((($pea/10))*$size)")
-        # y-position on composite
-        y=0
+    xvar=0
+    while [ ${xvar} -le 150 ]; do
+        # Get the x position
+        x=$(bc <<< "scale=0;((($xvar/10))*$size)")
+        # y-position on composite (bottom row)
+        y=$(bc <<< "scale=0;(((100/10))*$size)")
         # Place image on composite
         python ${camPath}/placeOnComp.py $composite "${base}.png" $x $y
-        # Increment xA
-        xa=$(( $xa + 10 ))
+        # Increment x-position
+        xvar=$(( $xvar + 10 ))
     done
 
+
+    # Remove B-colored image
+    rm ${base}.png
+    # Create A-colored image
+    ovitos ${camPath}/png_final_tstep.py $file 0 $size
+    # Place image on each PeB frame in top row
+    frames=0
+    while [ $frames -le 150 ]; do
+        # x-position depends on frame
+        x=$(bc <<< "scale=0;((($pea/10))*$size)")
+        # y-position is top of composite
+        y=0
+        # Place (pink) frame on composite
+        python ${camPath}/placeOnComp.py "pb${frames}.png" "${base}.png" $x $y
+        # Increment frame to place on
+        frames=$(( $frames + 10 ))
+    done
+
+    # Remove the A-colored image
+    rm ${base}.png
+    # Increment peB plane counter
     pea=$(( $pea + 10 ))
 
 done
