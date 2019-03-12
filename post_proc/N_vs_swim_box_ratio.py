10016c7cc4345ce50c79e97d9e7ad7b6f9d75857
Thomas Kolb <kolbt@live.unc.edu>
2019-03-12 15:07:03 -0400

    Plot to understand how activity and number of particles effect the ratio of persistence length to box size

--- post_proc/N_vs_swim_box_ratio.py
+++ post_proc/N_vs_swim_box_ratio.py
@@ -17,3 +17,35 @@ def nToLBox(N):
 
 def lp(Pe):
     return Pe / 3.0
+
+def lpBoxRatio(lp, box):
+    return float(lp) / float(box)
+
+pes = np.arange(0.0, 500.0, 0.01)
+ys1k = np.zeros_like(pes)
+ys10k = np.zeros_like(pes)
+ys100k = np.zeros_like(pes)
+ys1m = np.zeros_like(pes)
+
+l_box1k = nToLBox(1000)
+l_box10k = nToLBox(10000)
+l_box100k = nToLBox(100000)
+l_box1m = nToLBox(1000000)
+for i in xrange(len(pes)):
+    ys1k[i] = lpBoxRatio(lp(pes[i]), l_box1k)
+    ys10k[i] = lpBoxRatio(lp(pes[i]), l_box10k)
+    ys100k[i] = lpBoxRatio(lp(pes[i]), l_box100k)
+    ys1m[i] = lpBoxRatio(lp(pes[i]), l_box1m)
+
+# We want to plot this relationship at constant N values
+plt.plot(pes, ys1k, label=r'$N=1000$')
+plt.plot(pes, ys10k, label=r'$N=10000$')
+plt.plot(pes, ys100k, label=r'$N=100000$')
+plt.plot(pes, ys1m, label=r'$N=1000000$')
+plt.xlabel(r'Peclet Number $(Pe)$')
+plt.ylabel(r'Ratio of $l_{p}:l_{box}$')
+plt.xlim(0.0, 500.0)
+plt.ylim(0.0, 0.5)
+plt.legend()
+plt.savefig('N_vs_lp_to_box_ratio.png', dpi=1000)
+plt.close()
