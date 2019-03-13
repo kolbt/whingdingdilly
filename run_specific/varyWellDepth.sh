ea16b73a94f465fbb7336a31af881ece889d9da4
Thomas Kolb <kolbt@live.unc.edu>
2019-03-13 09:48:21 -0400

    Change template file

--- run_specific/varyWellDepth.sh
+++ run_specific/varyWellDepth.sh
@@ -10,14 +10,14 @@ if [ $answer == "y" ]; then
     hoomd_path='/nas/longleaf/home/kolbt/programs/cpu-hoomd/hoomd-blue/build'
     gsd_path='/nas/longleaf/home/kolbt/programs/gsd/build'
     script_path='/nas/longleaf/home/kolbt/whingdingdilly/run.sh'
-    template='/nas/longleaf/home/kolbt/whingdingdilly/run_specific/convergence_study.py'
+    template='/nas/longleaf/home/kolbt/whingdingdilly/run_specific/varyWellDepth.py'
     sedtype='sed'
     submit='sbatch'
 else
     hoomd_path='/Users/kolbt/Desktop/compiled/hoomd-blue_11_8_17/hoomd-blue/build'
     gsd_path='/Users/kolbt/Desktop/compiled/gsd/build'
     script_path='/Users/kolbt/Desktop/compiled/whingdingdilly/run.sh'
-    template='/Users/kolbt/Desktop/compiled/whingdingdilly/run_specific/convergence_study.py'
+    template='/Users/kolbt/Desktop/compiled/whingdingdilly/run_specific/varyWellDepth.py'
     sedtype='gsed'
     submit='sh'
 fi
