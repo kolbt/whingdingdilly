db538dccc4cc576f75dcf88fb52a5b909cda5b37
Thomas Kolb <kolbt@live.unc.edu>
2019-04-15 09:12:32 -0400

    Cut fat from rerun script

--- run_specific/rerun-dir.sh
+++ run_specific/rerun-dir.sh
@@ -1,24 +1,13 @@
 #!/bin/sh
 
-current=$( date "+%m_%d_%y" )
-this_path=$( pwd )
-
 echo "Are you running on Longleaf (y/n)?"
 read answer
 
 if [ $answer == "y" ]; then
-    hoomd_path='/nas/longleaf/home/kolbt/programs/cpu-hoomd/hoomd-blue/build'
-    gsd_path='/nas/longleaf/home/kolbt/programs/gsd/build'
     script_path='/nas/longleaf/home/kolbt/whingdingdilly/run.sh'
-    template='/nas/longleaf/home/kolbt/whingdingdilly/run_specific/vary_v_and_eps.py'
-    sedtype='sed'
     submit='sbatch'
 else
-    hoomd_path='/Users/kolbt/Desktop/compiled/hoomd-blue_11_8_17/hoomd-blue/build'
-    gsd_path='/Users/kolbt/Desktop/compiled/gsd/build'
     script_path='/Users/kolbt/Desktop/compiled/whingdingdilly/run.sh'
-    template='/Users/kolbt/Desktop/compiled/whingdingdilly/run_specific/vary_v_and_eps.py'
-    sedtype='gsed'
     submit='sh'
 fi
 
@@ -26,7 +15,6 @@ echo "GPU (y/n)?"
 read gpu
 
 if [ $gpu == "y" ]; then
-    hoomd_path='/nas/longleaf/home/kolbt/programs/hoomd_2.2.1/hoomd-blue/build'
     script_path='/nas/longleaf/home/kolbt/whingdingdilly/run_gpu.sh'
 fi
 
