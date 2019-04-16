d06e968430819b036524cc130856f3f969b5e568
Thomas Kolb <kolbt@live.unc.edu>
2019-04-16 14:51:54 -0400

    Writing theory and plotting for Brady theory

---
+++ phase_diagrammer/refine_phase_fig.py
@@ -0,0 +1,87 @@
+import sys
+import matplotlib
+#matplotlib.use('Agg')
+import matplotlib as mpl
+import matplotlib.pyplot as plt
+from matplotlib.ticker import MultipleLocator
+import numpy as np
+import pandas as pd
+
+## Here are my rc parameters for matplotlib
+mpl.rc('font', serif='Helvetica Neue')
+mpl.rcParams.update({'font.size': 18})
+#mpl.rcParams['figure.figsize'] = 3.2, 2.8
+mpl.rcParams['figure.dpi'] = 1000
+mpl.rcParams['axes.linewidth'] = 1.5
+mpl.rcParams['xtick.direction'] = 'in'
+mpl.rcParams['ytick.direction'] = 'in'
+mpl.rcParams['lines.linewidth'] = 2.0
+
+mpl.rcParams['xtick.major.size'] = 5
+mpl.rcParams['xtick.major.width'] = 1.5
+mpl.rcParams['xtick.minor.size'] = 3
+mpl.rcParams['xtick.minor.width'] = 1.5
+mpl.rcParams['ytick.major.size'] = 5
+mpl.rcParams['ytick.major.width'] = 1.5
+mpl.rcParams['ytick.minor.size'] = 3
+mpl.rcParams['ytick.minor.width'] = 1.5
+
+xminor = MultipleLocator(10)
+xmajor = MultipleLocator(50)
+yminor = MultipleLocator(0.1)
+ymajor = MultipleLocator(0.5)
+
+# Theory fits 99.3% of the data
+kappa = 3.8
+#kappa = 4.05
+phi_min = 0.6
+
+def solvePartFrac(PeA, PeB):
+    xA = ((3 * (np.pi**2) * kappa) - (4 * phi_min * PeB)) / ((4 * phi_min) * (PeA - PeB))
+    if xA <= 0.0:
+        xA = 5
+    return xA
+
+x = np.arange(0.0, 160.0, 0.001)
+y = np.zeros_like(x)
+
+file = 'hard_sphere_phase_separation.csv'
+ps_data = pd.read_csv(file)
+ps_data['xA'] /= 100.0
+
+distinctPePlanes = []
+for i in xrange(len(ps_data['PeB'])):
+    if ps_data['PeB'][i] not in distinctPePlanes:
+        distinctPePlanes.append(ps_data['PeB'][i])
+
+count = 1
+for i in xrange(len(distinctPePlanes)):
+    for j in xrange(len(ps_data['PeB'])):
+        # If data belongs to this plane, plot it
+        if ps_data['PeB'][j] == distinctPePlanes[i]:
+            if ps_data['PS'][j] == 1:
+                plt.scatter(ps_data['PeA'][j], ps_data['xA'][j], c='k')
+            else:
+                plt.scatter(ps_data['PeA'][j], ps_data['xA'][j], facecolor = 'none', edgecolor = 'k')
+    for j in xrange(len(y)):
+        y[j] = solvePartFrac(x[j], distinctPePlanes[i])
+    plt.plot(x, y, c='k')
+    plt.title(r'$Pe_{B}=$' + str(distinctPePlanes[i]), fontsize=30, y=1.02)
+    plt.xlabel(r'$Pe_{A}$')
+    plt.ylabel('$x_{A}$')
+    plt.xlim((-5, 155.0))
+    plt.ylim((-0.05, 1.05))
+
+    plt.axes().xaxis.set_minor_locator(xminor)
+    plt.axes().yaxis.set_minor_locator(yminor)
+    plt.axes().xaxis.set_major_locator(xmajor)
+    plt.axes().yaxis.set_major_locator(ymajor)
+
+    plt.tight_layout()
+    plt.savefig('HS_peB_' + str(distinctPePlanes[i]) + '.png' ,dpi=1000)
+    plt.savefig('HS_peB_' + str(count) + '.png' ,dpi=1000)
+    plt.close()
+
+    count += 1
+
+
