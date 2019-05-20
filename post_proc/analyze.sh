f7f0db9cd0618cfec0a7a8fe6ed9f13aafb91a85
Thomas Kolb <kolbt@live.unc.edu>
2019-05-20 14:01:26 -0400

    Run post proc on mono files

--- post_proc/analyze.sh
+++ post_proc/analyze.sh
@@ -12,7 +12,7 @@ hoomd_path=$4
 gsd_path=$5
 script_path=$6
 ep=$7
-phi=$8
+#phi=$8
 
 #python $script_path/nearest_neigh_small_array.py $pa $pb $xa $hoomd_path $gsd_path
 #python $script_path/nearest_neigh.py $pa $pb $xa $hoomd_path $gsd_path
@@ -30,16 +30,16 @@ phi=$8
 #python $script_path/dense_CoM.py $pa $pb $xa $hoomd_path $gsd_path
 #python $script_path/number_densities.py $pa $pb $xa $hoomd_path $gsd_path
 #python $script_path/force_diff_sources.py $pa $pb $xa $hoomd_path $gsd_path
-python $script_path/histogram_distance.py $pa $pb $xa $hoomd_path $gsd_path $ep $phi
+python $script_path/histogram_distance.py $pa $pb $xa $hoomd_path $gsd_path $ep
 #python $script_path/plotNumberDensities.py $pa $pb $xa
 #python $script_path/pairCorrelationRelations.py $pa $pb $xa $hoomd_path $gsd_path
 #python $script_path/mesh_nearest_neighbor.py $pa $pb $xa $hoomd_path $gsd_path
 #python $script_path/computeRDF.py $pa $pb $xa $hoomd_path $gsd_path $ep
 #python $script_path/heatmapType.py $pa $pb $xa $hoomd_path $gsd_path $ep
 #python $script_path/extrinsic_txt.py $pa $pb $xa $hoomd_path $gsd_path $ep
-#python $script_path/extrinsic_all_time.py $pa $pb $xa $hoomd_path $gsd_path $ep
-python $script_path/phi_extrinsic_all_time.py $pa $pb $xa $hoomd_path $gsd_path $ep $phi
-#python $script_path/sim_to_txt_no_cutoff.py $pa $pb $xa $hoomd_path $gsd_path $ep
+python $script_path/extrinsic_all_time.py $pa $pb $xa $hoomd_path $gsd_path $ep
+#python $script_path/phi_extrinsic_all_time.py $pa $pb $xa $hoomd_path $gsd_path $ep $phi
+python $script_path/sim_to_txt_no_cutoff.py $pa $pb $xa $hoomd_path $gsd_path $ep
 #python $script_path/convergence_analysis.py $pa $pb $xa $hoomd_path $gsd_path $ep $num
 #python $script_path/extrinsic_all_time_back_compat.py $pa $pb $xa $hoomd_path $gsd_path $ep
 #python $script_path/edge_detection.py $pa $pb $xa $hoomd_path $gsd_path
