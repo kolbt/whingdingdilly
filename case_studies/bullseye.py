d730fd78922355a1f753cc5dd4f38b40e4bf0396
Thomas Kolb <kolbt@live.unc.edu>
2019-10-17 13:50:29 -0400

    Initialize an active bullseye with variable types of particles and widths of active layers

--- case_studies/bullseye.py
+++ case_studies/bullseye.py
@@ -1,22 +1,16 @@
 '''
 #                        This is an 80 character line                          #
-We want to simulate soft spheres pushing through a stationary HCP wall...
 
-    Reads in: 1.) # of columns in HCP phase
-              2.) # of rows in HCP phase
-              3.) lattice spacing
-              4.) the swim force (for the ballistic particle)
-              
-    and runs a simulation accordingly :) Use this to sweep through
-    lattice spacing vs force. Plot to understand penetration of faster
-    particles into soft sphere dense phase.
 '''
 # Initial imports
 import sys
 import os
 
+# Run locally
 sys.path.append('/Users/kolbt/Desktop/compiled/hoomd-blue/build')
+# Run on the cpu
 #sys.path.append('/nas/longleaf/home/kolbt/programs/cpu-hoomd/hoomd-blue/build')
+# Run on the gpu
 sys.path.append('/nas/longleaf/home/kolbt/programs/hoomd_2.2.1/hoomd-blue/build')
 
 import hoomd
@@ -25,6 +19,8 @@ from hoomd import dem
 from hoomd import deprecated
 from hoomd import data
 import numpy as np
+
+import matplotlib.pyplot as plt
 
 # Set some constants
 kT = 1.0                        # temperature
@@ -43,22 +39,24 @@ def computeTauLJ(epsilon):
 eps = kT                                # repulsive depth
 tauLJ = computeTauLJ(eps)               # LJ time unit
 dt = 0.000001 * tauLJ                   # timestep
-dumpPerBrownian = 25.                   # number of dumps per 1 tauB
-simLength = 20. * tauBrown              # how long to run (in tauBrown)
+dumpPerBrownian = 2500.                 # number of dumps per 1 tauB
+simLength = 0.5 * tauBrown              # how long to run (in tauBrown)
 totTsteps = int(simLength / dt)         # how many tsteps to run
 numDumps = simLength * dumpPerBrownian  # total number of frames dumped
 dumpFreq = totTsteps / numDumps         # normalized dump frequency
 dumpFreq = int(dumpFreq)                # ensure this is an integer
 seed = 71996                            # a random seed
-seed2= 2394                             # activity seed
+seed2 = 2394                            # orientation seed
+seed3 = 183                             # activity seed
 
 # Some parameters (from command line):
-pe1 = float(sys.argv[1])
-pe2 = float(sys.argv[2])
+pe1 = float(sys.argv[1])    # fed in first, bullseye
+pe2 = float(sys.argv[2])    # second, second rign (and so on)
 pe3 = float(sys.argv[3])
 r1 = float(sys.argv[4])
 w2 = float(sys.argv[5])
 w3 = float(sys.argv[6])
+rAlign = float(sys.argv[7])
 
 def computeLat(activity):
     "Get lattice spacing based on effective diameter"
@@ -70,6 +68,15 @@ def computeLat(activity):
 def computeDistance(x, y):
     return np.sqrt((x**2) + (y**2))
     
+def orientToOrigin(x, y, act):
+    "Using similar triangles to find sides"
+    x *= -1
+    y *= -1
+    hypRatio = act / np.sqrt(x**2 + y**2)
+    xAct = hypRatio * x
+    yAct = hypRatio * y
+    return xAct, yAct
+    
 # List of activities
 peList = [ pe1, pe2, pe3 ]
 # List of ring radii
@@ -77,16 +84,19 @@ rList = [ 0, r1, r1 + w2, r1 + w2 + w3 ]
 # List to store particle positions and types
 pos = []
 typ = []
+rOrient = []
+# z-value for simulation initialization
+z = 0.5
 
 for i in xrange(len(peList)):
     rMin = rList[i]             # starting distance for particle placement
     rMax = rList[i + 1]         # maximum distance for particle placement
-    lat = computeLat(pe)        # activity-dependent lattice spacing
+    lat = computeLat(peList[i]) # activity-dependent lattice spacing
     ver = np.sqrt(0.75) * lat   # vertical shift between lattice rows
     hor = lat / 2.0             # horizontal shift between lattice rows
     
-    x = rMin
-    y = rMin
+    x = 0
+    y = 0
     shift = 0
     while y < rMax:
         r = computeDistance(x, y)
@@ -96,7 +106,7 @@ for i in xrange(len(peList)):
             continue
             
         # Check if x-position is too large
-        if r >= rMax:
+        if r >= (rMax - (lat/2.)):
             y += ver
             shift += 1
             if shift % 2:
@@ -105,114 +115,127 @@ for i in xrange(len(peList)):
                 x = 0
             continue
         
+        # Whether or not particle is oriented
+        if r > (rMax - rAlign):
+            # Aligned
+            rOrient.append(1)
+        else:
+            # Random
+            rOrient.append(0)
+        
         # If the loop makes it this far, append
-        pos.append((x, y))
+        pos.append((x, y, z))
+        typ.append(i)
         if x != 0 and y != 0:
-            pos.append((-x, y))
-            pos.append((-x, -y))
-            pos.append((x, -y))
+            # Mirror positions, alignment and type
+            pos.append((-x, y, z))
+            pos.append((-x, -y, z))
+            pos.append((x, -y, z))
+            rOrient.append(rOrient[-1])
+            rOrient.append(rOrient[-1])
+            rOrient.append(rOrient[-1])
+            typ.append(i)
+            typ.append(i)
+            typ.append(i)
         # y must be zero
         elif x != 0:
-            pos.append((x, -y))
+            pos.append((-x, y, z))
+            rOrient.append(rOrient[-1])
+            typ.append(i)
         # x must be zero
         elif y!= 0:
-            pos.append((-x, y))
-        
-            
-        
-        
-        
+            pos.append((x, -y, z))
+            rOrient.append(rOrient[-1])
+            typ.append(i)
+         
+        # Increment counter
+        x += lat
+
+# Get each coordinate in a list
+x, y, z = zip(*pos)
+
+## Plot as scatter
+#cs = np.divide(typ, float(len(peList)))
+#cs = rOrient
+#plt.scatter(x, y, s=2., c=cs, cmap='jet')
+#ax = plt.gca()
+#ax.set_aspect('equal')
+#plt.show()
+
+partNum = len(pos)
+xBox = 3. * (max(x))
+yBox = xBox
+# Get the number of types
+uniqueTyp = []
+for i in typ:
+    if i not in uniqueTyp:
+        uniqueTyp.append(i)
+# Get the number of each type
+particles = [ 0 for x in xrange(len(uniqueTyp)) ]
+for i in xrange(len(uniqueTyp)):
+    for j in typ:
+        if uniqueTyp[i] == j:
+            particles[i] += 1
+# Convert types to letter values
+unique_char_types = []
+for i in uniqueTyp:
+    unique_char_types.append( chr(ord('@') + i+1) )
+char_types = []
+for i in typ:
+    char_types.append( chr(ord('@') + i+1) )
     
-    
-
-# Make the mesh find positions for the HCP particles
-pos = []
-y = 0
-for i in xrange(nRow):
-    # Every other row needs to be shifted in the x direction
-    if i % 2 == 0:
-        x = 0.0
-    else:
-        x = hor
-
-    for j in xrange(nCol):
-        pos.append([x, y, 0.5]) # place particle
-        x += lat                # move a lattice spacing to the right
-
-    # Every row increases by the same y-value
-    y += ver
-
-nHex = len(pos)     # number of lattice particles
-partNum = nHex + 1  # particle we are testing
+# Get a list of activities for all particles
+pe = []
+for i in typ:
+    pe.append(peList[i])
 
 # Now we make the system in hoomd
 hoomd.context.initialize()
 # A small shift to help with the periodic box
-per = ver
 snap = hoomd.data.make_snapshot(N = partNum,
-                                box = hoomd.data.boxdim(Lx=xBox + per,
-                                                        Ly=yBox + per,
+                                box = hoomd.data.boxdim(Lx=xBox,
+                                                        Ly=yBox,
                                                         dimensions=2),
-                                particle_types = ['A', 'B'])
-
-# Get position for subject particle
-xSub = ((nCol * lat) - hor) + 1.
-ySub = yBox / 2.0
-subject = [xSub, ySub, 0.5]
-pos.append(subject)
-
-# You have to shift the positions
-xShift = xBox / 2.0
-yShift = yBox / 2.0
-for i in xrange(len(pos)):
-    pos[i][0] -= xShift
-    pos[i][1] -= yShift
+                                particle_types = unique_char_types)
 
 # Set positions/types for all particles
 snap.particles.position[:] = pos[:]
-snap.particles.typeid[:] = 0
-snap.particles.types[:] = 'A'
-snap.particles.typeid[-1] = 1
-snap.particles.types[-1] = 'B'
+snap.particles.typeid[:] = typ[:]
+snap.particles.types[:] = char_types[:]
 
 # Initialize the system
 system = hoomd.init.read_snapshot(snap)
 all = hoomd.group.all()
-hcp = hoomd.group.type(type='A')
-active = hoomd.group.type(type='B')
+groups = []
+for i in unique_char_types:
+    groups.append(hoomd.group.type(type=i))
 
 # Set particle potentials
 nl = hoomd.md.nlist.cell()
 lj = hoomd.md.pair.lj(r_cut=2**(1/6), nlist=nl)
 lj.set_params(mode='shift')
-lj.pair_coeff.set('A', 'A', epsilon=eps, sigma=sigma)
-lj.pair_coeff.set('A', 'B', epsilon=eps, sigma=sigma)
-lj.pair_coeff.set('B', 'B', epsilon=eps, sigma=sigma)
+for i in xrange(len(unique_char_types)):
+    for j in range(i, len(unique_char_types)):
+        lj.pair_coeff.set(unique_char_types[i],
+                          unique_char_types[j],
+                          epsilon=eps, sigma=sigma)
 
-# Add a wall potential that acts on the HCP particles
-wallShift = 5.0 * lat
-wallShift = 0.0
-xyzLWall = (((xBox + per) / -2.0), 0, 0)
-xyzRWall = ((nCol * lat) - xShift + wallShift, 0, 0)
-leftWall = hoomd.md.wall.group(hoomd.md.wall.plane(origin=xyzLWall,
-                                                   normal=(1,0,0),
-                                                   inside=True))
-rightWall = hoomd.md.wall.group(hoomd.md.wall.plane(origin=xyzRWall,
-                                                    normal=(1,0,0),
-                                                    inside=False))
-leftLJWall = hoomd.md.wall.slj(leftWall, r_cut=1.112)
-rightLJWall = hoomd.md.wall.slj(rightWall, r_cut=1.112)
-leftLJWall.force_coeff.set('A', epsilon=10.0, sigma=1.0)
-leftLJWall.force_coeff.set('B', epsilon=0.0, sigma=1.0)
-rightLJWall.force_coeff.set('A', epsilon=10.0, sigma=1.0)
-rightLJWall.force_coeff.set('B', epsilon=0.0, sigma=1.0)
-
-# Active motion initially oriented towards the HCP phase
+# Set activity of each group
+np.random.seed(seed2)                           # seed for random orientations
+angle = np.random.rand(partNum) * 2 * np.pi     # random particle orientation
 activity = []
-tuple = (-swimForce, 0, 0)
-activity.append(tuple)
-hoomd.md.force.active(group=active,
-                      seed=seed2,
+for i in xrange(partNum):
+    if rOrient[i] == 0:
+        x = (np.cos(angle[i])) * pe[i]
+        y = (np.sin(angle[i])) * pe[i]
+    else:
+        x, y = orientToOrigin(pos[i][0], pos[i][1], pe[i])
+    z = 0.
+    tuple = (x, y, z)
+    activity.append(tuple)
+# Implement the activities in hoomd
+hoomd.md.force.active(group=all,
+                      seed=seed3,
                       f_lst=activity,
                       rotation_diff=D_r,
                       orientation_link=False,
@@ -222,7 +245,17 @@ hoomd.md.force.active(group=active,
 hoomd.md.integrate.mode_standard(dt=dt)
 bd = hoomd.md.integrate.brownian(group=all, kT=kT, seed=seed)
 
-out = "active_pe" + str(swimForce) + "_lattice" + str(lat) + ".gsd"
+# Name the file from parameters
+out = "bullseye_pe"
+for i in peList:
+    out += str(int(i))
+    out += "_"
+out += "r"
+for i in range(1, len(rList)):
+    out += str(int(rList[i]))
+    out += "_"
+    
+out += "rAlign_" + str(rAlign) + ".gsd"
 
 #write dump
 hoomd.dump.gsd(out,
