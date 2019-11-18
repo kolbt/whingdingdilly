86666a2efd447cdb84b7026362f59235f7a34cd0
Thomas Kolb <kolbt@live.unc.edu>
2019-11-18 12:30:45 -0500

    Increase memory allocated to jobs

--- case_studies/sbAnalyzeBulk.sh
+++ case_studies/sbAnalyzeBulk.sh
@@ -1,7 +1,8 @@
 #!/bin/sh
 #SBATCH -p general                          # partition to run on
 #SBATCH -n 1                                # number of cores
-#SBATCH -t 3-00:00                         # time (D-HH:MM)
+#SBATCH -t 3-00:00                          # time (D-HH:MM)
+#SBATCH --mem=10g
 
 # Command to increase memory allocated --mem=100g
 
