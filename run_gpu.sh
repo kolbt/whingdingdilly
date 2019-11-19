3b55f0895f9d8d1d6e9d06d4c3a030ba082b0f03
Thomas Kolb <kolbt@live.unc.edu>
2019-11-19 10:54:59 -0500

    Avoid faulty gpu g0601

--- run_gpu.sh
+++ run_gpu.sh
@@ -5,6 +5,7 @@
 #SBATCH --nodes=1                           # run on one node
 #SBATCH --time=6-00:00                      # time (D-HH:MM)
 #SBATCH --mem=20g
+#SBATCH --nodelist=g0602,g0603,g0604,g0605
 
 filename=$1
 
