2cfe5981c8c93d8c7116fb06ebeb175a783e76fb
Thomas Kolb <kolbt@live.unc.edu>
2019-12-04 13:39:22 -0500

    Need to sbatch file

--- case_studies/fullVideoAnalysis.sh
+++ case_studies/fullVideoAnalysis.sh
@@ -9,6 +9,6 @@ do
     # Input file is always of the form pe${peInner}_${peOuter}
     pein=$(echo $filename | $sedtype "s/^.*pe\([0-9]*\)_.*/\1/")
     peout=$(echo $filename | $sedtype "s/^.*pe${pein}_\([0-9]*\)_.*/\1/")
-    sh /nas/longleaf/home/kolbt/whingdingdilly/case_studies/sbfullVideoAnalysis.sh $filename $script_path $pein $peout
+    sbatch /nas/longleaf/home/kolbt/whingdingdilly/case_studies/sbfullVideoAnalysis.sh $filename $script_path $pein $peout
     
 done
