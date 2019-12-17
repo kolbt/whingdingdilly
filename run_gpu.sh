80b0401933244beaac185f8342a9cb207f68ce1a
Thomas Kolb <kolbt@live.unc.edu>
2019-12-17 16:02:01 -0500

    Failing on 0605 for some reason

--- run_gpu.sh
+++ run_gpu.sh
@@ -3,6 +3,7 @@
 #SBATCH --gres=gpu:1                        # I want one gpus
 #SBATCH --partition=gpu                     # partition to run on
 #SBATCH --time=6-00:00                      # time (D-HH:MM)
+#SBATCH --exclude=g0605
 
 filename=$1
 
