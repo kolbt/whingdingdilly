d079b8bde086c2e6ff2ddf7073168539454f3c8d
Thomas Kolb <kolbt@live.unc.edu>
2019-12-17 15:44:14 -0500

    Maybe they fixed g0601

--- run_gpu_exclusive.sh
+++ run_gpu_exclusive.sh
@@ -2,7 +2,10 @@
 #SBATCH --qos gpu_access                    # quality of service
 #SBATCH --gres=gpu:1                        # I want one gpus
 #SBATCH --partition=gpu                     # partition to run on
+#SBATCH --nodes=1                           # run on one node
 #SBATCH --time=6-00:00                      # time (D-HH:MM)
+#SBATCH --mem=20g
+#SBATCH --exclude=g0601
 
 filename=$1
 
