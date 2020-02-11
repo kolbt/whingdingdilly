a58f15246771de92f8393672efa7779f700513bc
Thomas Kolb <kolbt@live.unc.edu>
2020-02-11 09:09:17 -0500

    Update how post-processing reads in file data

--- post_proc/post_proc.sh
+++ post_proc/post_proc.sh
@@ -8,96 +8,20 @@ if [ $answer == "y" ]; then
     hoomd_path='/nas/longleaf/home/kolbt/programs/hoomd-blue/build'
     gsd_path='/nas/longleaf/home/kolbt/programs/gsd/build'
     script_path='/nas/longleaf/home/kolbt/whingdingdilly/post_proc'
-    sedtype='sed'
     submit='sbatch'
+    module add python/3.6.6
 else
     hoomd_path='/Users/kolbt/Desktop/compiled/hoomd-blue_11_8_17/hoomd-blue/build'
     gsd_path='/Users/kolbt/Desktop/compiled/gsd/build'
     script_path='/Users/kolbt/Desktop/compiled/whingdingdilly/post_proc'
-    sedtype='gsed'
     submit='sh'
 fi
 
-echo "Are you running on your laptop (y/n)?"
-read answer
+for file in $(ls *gsd)
+do
 
-if [ $answer == "y" ]; then
-    hoomd_path='/Users/kolbt/Desktop/compiled/hoomd-blue/build'
-fi
-
-echo "Epsilon in filename?"
-read isEps
-
-echo "Monodisperse simulation?"
-read isMono
-
-echo "Is epsilon a float?"
-read isFloat
-
-if [ $isMono == 'n' ]; then
-    if [ $isEps == 'n' ]; then
-        ep=$(( 1 ))
-        for filename in $( ls pa*.gsd )
-        #for filename in $( ls *pa*_0.png )
-        #for filename in $( ls all_pa*.txt )
-        do
-
-            # pull parameters from filename
-
-            pa=$(echo $filename | $sedtype 's/^.*pa\([0-9]*\)_.*/\1/')
-            pb=$(echo $filename | $sedtype 's/^.*pb\([0-9]*\)_.*/\1/')
-            xa=$(echo $filename | $sedtype 's/^.*xa\([0-9]*\)..*/\1/')
-
-            $submit $script_path/analyze.sh $pa $pb $xa $hoomd_path $gsd_path $script_path $ep
-
-        done
-
-    else
-        for filename in $( ls pa*.gsd )
-        do
-
-            # Pull parameters from filename
-            pa=$(echo $filename | $sedtype 's/^.*pa\([0-9]*\)_.*/\1/')
-            pb=$(echo $filename | $sedtype 's/^.*pb\([0-9]*\)_.*/\1/')
-            xa=$(echo $filename | $sedtype 's/^.*xa\([0-9]*\)_.*/\1/')
-            ep=$(echo $filename | $sedtype 's/^.*ep\([0-9]*\)..*/\1/')
-    #        phi=$(echo $filename | $sedtype 's/^.*phi\([0-9]*\)..*/\1/')
-    #        al=$(echo $filename | $sedtype 's/^.*alpha\([0-9]*\)..*/\1/')
-    #        num=$(echo $filename | $sedtype 's/^.*N\([0-9]*\)..*/\1/')
-
-    #        $submit $script_path/analyze.sh $pa $pb $xa $hoomd_path $gsd_path $script_path $ep $num
-            $submit $script_path/analyze.sh $pa $pb $xa $hoomd_path $gsd_path $script_path $ep $filename
-
-        done
-    fi
+    $submit $script_path/analyze.sh $hoomd_path $gsd_path $script_path $file
     
-else
-    if [ $isFloat == 'n' ]; then
-        for filename in $( ls pe*.gsd )
-        do
-        
-            pe=$(echo $filename | $sedtype 's/^.*pe\([0-9]*\)_.*/\1/')
-            ep=$(echo $filename | $sedtype 's/^.*ep\([0-9]*\)_.*/\1/')
-            phi=$(echo $filename | $sedtype 's/^.*phi\([0-9]*\)..*/\1/')
-            $submit $script_path/analyze.sh $pe 0 0 $hoomd_path $gsd_path $script_path $ep $filename $phi
-            
-        done
-        
-    else
-        # If epsilon is a float, you need to use this sed script
-        for filename in $( ls pe*.gsd )
-        do
-            pe=$(echo $filename | $sedtype 's/^.*pe\([0-9]*\)_.*/\1/')
-            ep=$(echo $filename | $sedtype 's/^.*[^0-9]\([0-9]*\.[0-9]*\)_phi.*$/\1/')
-            phi=$(echo $filename | $sedtype 's/^.*phi\([0-9]*\)..*/\1/')
-            $submit $script_path/analyze.sh $pe 0 0 $hoomd_path $gsd_path $script_path $ep $filename $phi
-        done
-    fi
+done
 
-fi
-
-#pa=100
-#pb=500
-#xa=50
-#$submit $script_path/analyze.sh $pa $pb $xa $hoomd_path $gsd_path $script_path
 
