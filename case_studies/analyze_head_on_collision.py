7498135642f92abbd216e9592c9812eb82ebb79a
Thomas Kolb <kolbt@live.unc.edu>
2020-02-28 14:56:47 -0500

    Ran a quick sim to figure out how body force contributes to a collision

---
+++ case_studies/analyze_head_on_collision.py
@@ -0,0 +1,64 @@
+'''
+#                           This is an 80 character line                       #
+What is the equilibrium separation of a head on collision?
+'''
+
+import sys
+
+gsd_path = '/nas/longleaf/home/kolbt/programs/gsd/build'
+sys.path.append(gsd_path)       # ensure gsd is in your python path
+gsd_path = '/Users/kolbt/Desktop/compiled/gsd/build'
+sys.path.append(gsd_path)       # ensure gsd is in your python path
+
+import gsd
+from gsd import hoomd
+from gsd import pygsd
+
+import freud
+from freud import parallel
+from freud import box
+from freud import density
+from freud import cluster
+
+import numpy as np
+import math
+from scipy import stats
+
+#import matplotlib
+#matplotlib.use('Agg')
+import matplotlib.pyplot as plt
+    
+# Base filename
+inFile = 'head_on_collision.gsd'
+f = hoomd.open(name=inFile, mode='rb')
+
+dump = int(f.__len__())         # get number of timesteps dumped
+start = 0                       # gives first frame to read
+end = dump                      # gives last frame to read
+box_data = np.zeros((1), dtype=np.ndarray)        # box dimensions
+
+# Access and read .gsd data
+with hoomd.open(name=inFile, mode='rb') as t:
+    snap = t[-1]
+    first_tstep = snap.configuration.step
+    box_data = snap.configuration.box
+    # Box dimensions
+    x_box = box_data[0]
+    y_box = box_data[1]
+    hx_box = x_box / 2.0
+    hy_box = y_box / 2.0
+    a_box = x_box * y_box
+    
+    # Get the positions
+    pos = snap.particles.position[:]
+    delta = pos[1][0] - pos[0][0]
+    print(delta)
+
+# Now let's see what force this corresponds to
+def ljForce(r, eps=1., sigma=1.):
+    div = (sigma / r)
+    dU = (24. * eps / r) * (2*(div**12) - (div**6))
+    return dU
+    
+ljF = ljForce(delta)
+print("The Lennard-Jones force at equilibrium separation: ", ljF)
---
+++ case_studies/head_on_collision.py
@@ -0,0 +1,107 @@
+'''
+#                           This is an 80 character line                       #
+We want to answer the question of what the equilibrium separation of two active
+particles oriented at one another should be...
+'''
+
+import sys
+
+# Run locally
+sys.path.append('/Users/kolbt/Desktop/compiled/hoomd-blue/build')
+
+import hoomd
+from hoomd import md
+from hoomd import dem
+from hoomd import deprecated
+from hoomd import data
+
+import matplotlib
+#matplotlib.use('Agg')
+import matplotlib.pyplot as plt
+
+# Set some constants
+kT = 1.0                        # temperature
+threeEtaPiSigma = 1.0           # drag coefficient
+sigma = 1.0                     # particle diameter
+D_t = kT / threeEtaPiSigma      # translational diffusion constant
+D_r = (3.0 * D_t) / (sigma**2)  # rotational diffusion constant
+tauBrown = (sigma**2) / D_t     # brownian time scale (invariant)
+
+# Repulsion and timestep
+def computeTauLJ(epsilon):
+    "Given epsilon, compute lennard-jones time unit"
+    tauLJ = ((sigma**2) * threeEtaPiSigma)
+    return tauLJ
+    
+eps = kT                                # repulsive depth
+tauLJ = computeTauLJ(eps)               # LJ time unit
+dt = 0.000001 * tauLJ                   # timestep
+dumpPerBrownian = 1.                   # number of dumps per 1 tauB
+simLength = 1.0 * tauBrown            # how long to run (in tauBrown)
+totTsteps = int(simLength / dt)         # how many tsteps to run
+numDumps = simLength * dumpPerBrownian  # total number of frames dumped
+dumpFreq = totTsteps / numDumps         # normalized dump frequency
+dumpFreq = int(dumpFreq)                # ensure this is an integer
+seed = 71996                            # a random seed
+seed2 = 2394                            # orientation seed
+seed3 = 183                             # activity seed
+
+# Some parameters:
+
+# Number of particles
+N = 2.
+# Particle positions
+pos = [[-0.5, 0., 0.5], [0.5, 0., 0.5]]
+# Types
+typ = [0, 0]
+
+
+# Now we make the system in hoomd
+hoomd.context.initialize()
+# A small shift to help with the periodic box
+snap = hoomd.data.make_snapshot(N = 2,
+                                box = hoomd.data.boxdim(Lx=10.,
+                                                        Ly=10.,
+                                                        dimensions=2),
+                                particle_types = ['A'])
+
+# Set positions/types for all particles
+snap.particles.position[:] = pos[:]
+snap.particles.typeid[:] = typ[:]
+
+# Initialize the system
+system = hoomd.init.read_snapshot(snap)
+all = hoomd.group.all()
+
+# Set particle potentials
+nl = hoomd.md.nlist.cell()
+lj = hoomd.md.pair.lj(r_cut=2**(1/6), nlist=nl)
+lj.set_params(mode='shift')
+lj.pair_coeff.set('A', 'A', epsilon=eps, sigma=1.)
+
+# Brownian integration
+hoomd.md.integrate.mode_standard(dt=dt)
+bd = hoomd.md.integrate.brownian(group=all, kT=0., seed=seed)
+
+# Set body force of each particle explicity
+F_act = 500.
+aForce = (F_act, 0., 0.)
+bForce = (-F_act, 0., 0.)
+# Implement the activities in hoomd
+agroup = hoomd.group.tag_list(name="a", tags=[0])
+bgroup = hoomd.group.tag_list(name="b", tags=[1])
+hoomd.md.force.constant(fvec=aForce, group=agroup)
+hoomd.md.force.constant(fvec=bForce, group=bgroup)
+
+# Write dump
+hoomd.dump.gsd('head_on_collision.gsd',
+               period=20000,
+               group=all,
+               overwrite=True,
+               phase=-1,
+               dynamic=['attribute', 'property', 'momentum'])
+
+# Run
+hoomd.run(totTsteps)
+
+
