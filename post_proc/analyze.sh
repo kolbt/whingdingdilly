a26c6c56afc9c57604f20fe3fa0c898716e8764d
Thomas Kolb <kolbt@live.unc.edu>
2020-03-13 10:25:22 -0400

    Run analysis scripts on newly completed epsilon sweep, see if this matches with anallytical prediction

--- post_proc/analyze.sh
+++ post_proc/analyze.sh
@@ -85,7 +85,7 @@ dtau=${pass[7]}
 #python3 $script_path/edges_from_bins.py $fname $pe $pb $xa $ep $phi
 #python3 $script_path/indiv_cluster_pressure.py $fname $pe $pb $xa $ep $phi $tau
 python3 $script_path/histogram-densities.py $fname $pe $pb $xa $ep $phi $tau
-#python3 $script_path/interparticle_pressure.py $fname $pe $pb $xa $ep $phi $tau
+python3 $script_path/interparticle_pressure.py $fname $pe $pb $xa $ep $phi $tau
 python3 $script_path/image_final_tstep.py $fname $pe $pb $xa $ep $phi $tau
 
 
