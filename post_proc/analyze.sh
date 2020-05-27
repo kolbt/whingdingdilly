a2b8b16ce0580c932a6488e541d73b43e5c6a25c
Thomas Kolb <kolbt@live.unc.edu>
2020-05-27 18:44:53 -0400

    Color system by radial distance, max at edge of cluster

--- post_proc/analyze.sh
+++ post_proc/analyze.sh
@@ -82,8 +82,8 @@ dtau=${pass[7]}
 #python3 $script_path/edge_distance.py $fname $pe $pb $xa $ep $phi
 #python3 $script_path/edges_from_bins.py $fname $pe $pb $xa $ep $phi
 #python3 $script_path/indiv_cluster_pressure.py $fname $pe $pb $xa $ep $phi $tau
-python3 $script_path/histogram-densities.py $fname $pa $pb $xa $ep $phi $dtau
-python3 $script_path/interparticle_pressure.py $fname $pa $pb $xa $ep $phi $dtau
+#python3 $script_path/histogram-densities.py $fname $pa $pb $xa $ep $phi $dtau
+#python3 $script_path/interparticle_pressure.py $fname $pa $pb $xa $ep $phi $dtau
 #python3 $script_path/image_final_tstep.py $fname $pe $pb $xa $ep $phi $tau
 #python3 $script_path/image_single_particle.py $fname $pe $pb $xa $ep $phi $tau
 #python3 $script_path/sim_frames.py $fname $pe $pb $xa $ep $phi $tau
@@ -92,13 +92,14 @@ python3 $script_path/interparticle_pressure.py $fname $pa $pb $xa $ep $phi $dtau
 #python3 $script_path/plot_particles_and_edge.py $fname $pe $pb $xa $ep $phi $tau
 #python3 $script_path/soft_nearest_neighbors.py $pe $pb $xa $hoomd_path $gsd_path $ep $fname
 #python3 $script_path/delta_spatial.py $pe $pb $xa $hoomd_path $gsd_path $ep $fname
+python3 $script_path/radial_color.py $fname $pe $pb $xa $ep $phi $dtau
 
 # Video for ellipse collection raw simulation
 #outf=${fname::-4}
-#outf=${fname%????}
-#ffmpeg -start_number 0 -framerate 20 -i ${outf}_frame_%04d.png\
-# -vcodec libx264 -s 562x562 -pix_fmt yuv420p -threads 1\
-# ${outf}.mp4
+outf=${fname%????}
+ffmpeg -start_number 0 -framerate 20 -i ${outf}_frame_%04d.png\
+ -vcodec libx264 -s 960x720 -pix_fmt yuv420p -threads 1\
+ ${outf}.mp4
 
 ## Videos for seminar
 #outf=${fname::-4}
