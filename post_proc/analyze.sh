fd1a1669eb082870c1001bf26005f47071757de2
Thomas Kolb <kolbt@live.unc.edu>
2020-06-27 11:25:21 -0400

    Cdata on all sims

--- post_proc/analyze.sh
+++ post_proc/analyze.sh
@@ -86,7 +86,7 @@ dtau=${pass[7]}
 #python3 $script_path/interparticle_pressure.py $fname $pa $pb $xa $ep $phi $dtau
 #python3 $script_path/image_final_tstep.py $fname $pe $pb $xa $ep $phi $tau
 #python3 $script_path/image_single_particle.py $fname $pe $pb $xa $ep $phi $tau
-python3 $script_path/sim_frames.py $fname $pe $pb $xa $ep $phi $tau
+#python3 $script_path/sim_frames.py $fname $pe $pb $xa $ep $phi $tau
 #python3 $script_path/sim_velocity.py $fname $pe $pb $xa $ep $phi $tau
 #python3 $script_path/sim_orientation.py $fname $pe $pb $xa $ep $phi $tau
 #python3 $script_path/plot_particles_and_edge.py $fname $pe $pb $xa $ep $phi $tau
@@ -111,10 +111,10 @@ python3 $script_path/cdata.py $fname $pe $pb $xa $ep $phi $dtau
 
 # Videos for seminar
 #outf=${fname::-4}
-outf=${fname%????}
-ffmpeg -start_number 0 -framerate 20 -i ${outf}_frame_%04d.png\
- -vcodec libx264 -s 562x562 -pix_fmt yuv420p -threads 1\
- ${outf}.mp4
+#outf=${fname%????}
+#ffmpeg -start_number 0 -framerate 20 -i ${outf}_frame_%04d.png\
+# -vcodec libx264 -s 562x562 -pix_fmt yuv420p -threads 1\
+# ${outf}.mp4
 
 ## Movie for single particle motion
 #ffmpeg -start_number 0 -framerate 20 -i test_fm%04d.png\
